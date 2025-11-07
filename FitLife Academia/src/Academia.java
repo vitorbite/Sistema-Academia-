1279c16451889ca7f7f05cc17edfdf7891fd4662
vitorbite <vitorbitencourt20@gmail.com>
2025-11-06 23:45:26 -0300

    Criando a classe Academia

---
+++ FitLife Academia/src/Academia.java
@@ -0,0 +1,15 @@
+public class Academia {
+    private Aluno alunos[];
+    private Professor professores[];
+
+    public void CadastrarAlunos(Aluno aluno){
+
+    }
+    public void CadastrarProfessor(Professor professor){
+
+    }
+    public void VerHistoricoDeTreinos(){
+
+    }
+    
+}
