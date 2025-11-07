c0879c7215410f31a8d3d9999ab6624a42454280
vitorbite <vitorbitencourt20@gmail.com>
2025-11-06 23:40:42 -0300

    Adicionando as classes de Aluno e Professor

---
+++ FitLife Academia/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    private String plano;
+    private int frequencia;
+    private Modalidade modalidades[];
+
+    public void inscreverEmModalidade(Modalidade modalidade){
+
+    }
+    //adicionar Getters e Setters necessários
+
+}
