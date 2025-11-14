a42e96a947cd5c9aba698bff9c9b03f41363d8f0
Reginaldo Carvalho <arthurbr120@hotmail.com>
2025-11-14 16:30:58 -0300

    Alterando o arquivo Academia.Java (VerHIstóricoTreinos)

--- FitLife Academia/src/Academia.java
+++ FitLife Academia/src/Academia.java
@@ -9,7 +9,17 @@ public class Academia {
 
     }
     public void VerHistoricoDeTreinos(){
+        System.out.println("--- Histórico de Treinos de " + aluno.getNome() + " ---");
+            if (aluno.getHistoricoTreinos().isEmpty()) {
+            System.out.println("Nenhum registro de treino encontrado.");
+            return;
+        }
 
+            for (RegistroTreino registro : aluno.getHistoricoTreinos()) {
+        System.out.println("----------------------------------------");
+        System.out.println(registro.toString());
+        }
+        System.out.println("----------------------------------------");
     }
     
 }
