e6ea06739b9cdc3f3150e2505b524d95e328beeb
Reginaldo Carvalho <arthurbr120@hotmail.com>
2025-11-14 16:44:37 -0300

    Adicionando Herança(Modalidade) as classes Musculação/Pilates/Yoga

--- FitLife Academia/src/Modalidade.java
+++ FitLife Academia/src/Modalidade.java
@@ -1,7 +1,9 @@
 public abstract class Modalidade {
     private String horarios[];
     private Professor professores[];
-    
+
     // As modalidades Yoga, Muculação e Pilates herdarão esses atributos
+
+
 }
 
