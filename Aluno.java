12a051352ac778664a92456471f9c698eb2c9e1c
ksouvu <canutoisael@gmail.com>
2025-11-18 18:24:44 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,74 @@
+import Planos.Plano;
+
+public class Aluno extends Pessoa {
+
+    private Plano plano;
+    private int frequencia = 0;
+    private Modalidade modalidades[];
+
+    private static final int MAX_MODALIDADES = 10;
+
+    public Aluno(String nome, String cpf, Plano plano) {
+        super(nome, cpf);
+        this.plano = plano;
+        this.modalidades = new Modalidade[MAX_MODALIDADES];
+    }
+
+    @Override
+    public String getTipo() {
+        return plano.eVIP() ? "ALUNO VIP" : "ALUNO";
+    }
+
+    public void inscreverEmModalidade(Modalidade modalidade) {
+
+        if (modalidade == null) return;
+
+        for (int i = 0; i < modalidades.length; i++) {
+            if (modalidades[i] == null) {
+                modalidades[i] = modalidade;
+                return;
+            }
+        }
+
+        System.out.println("Não foi possível inscrever: limite de modalidades atingido.");
+    }
+
+    public void alertaDeFrequenciaBaixa() {
+        int duracao = plano.getDuracao_em_dias();
+
+        if (duracao <= 0) {
+            System.out.println("Plano com duração inválida.");
+            return;
+        }
+
+        double media = (double) frequencia / duracao;
+
+        if (media <= 0.5) {
+            System.out.println("Frequência Baixa!\nBora Treinar!");
+        }
+    }
+
+    public Plano getPlano() {
+        return plano;
+    }
+
+    public void setPlano(Plano plano) {
+        this.plano = plano;
+    }
+
+    public int getFrequencia() {
+        return frequencia;
+    }
+
+    public void setFrequencia(int frequencia) {
+        this.frequencia = frequencia;
+    }
+
+    public Modalidade[] getModalidades() {
+        return modalidades;
+    }
+
+    public boolean isVip() {
+        return plano.eVIP();
+    }
+}
