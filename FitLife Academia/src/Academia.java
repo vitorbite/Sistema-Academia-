5c6740992030ba9a69af4f0c4d272b843372d55e
vitorbite <vitorbitencourt20@gmail.com>
2025-11-18 19:17:20 -0300

    fix: Corrigindo arquivos commitados fora da pasta src

--- FitLife Academia/src/Academia.java
+++ FitLife Academia/src/Academia.java
@@ -2,12 +2,6 @@ public class Academia {
     private Aluno alunos[];
     private Professor professores[];
 
-    public void CadastrarAlunos(Aluno aluno){
-
-    }
-    public void CadastrarProfessor(Professor professor){
-
-    }
     public void VerHistoricoDeTreinos(){
         System.out.println("--- Histórico de Treinos de " + aluno.getNome() + " ---");
             if (aluno.getHistoricoTreinos().isEmpty()) {
--- FitLife Academia/src/Professor.java
+++ FitLife Academia/src/Professor.java
@@ -1,3 +1,39 @@
-public class Professor {
-    private String nome;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Pessoa {
+
+    private List<String> tiposExercicio;
+    private List<String> horarios;
+
+    public Professor(String nome, String cpf) {
+        super(nome, cpf);
+        this.tiposExercicio = new ArrayList<>();
+        this.horarios = new ArrayList<>();
+    }
+
+    @Override
+    public String getTipo() {
+        return "professor";
+    }
+
+    public void adicionarTipoExercicio(String tipo) {
+        if (tipo != null && !tipo.isEmpty()) {
+            tiposExercicio.add(tipo);
+        }
+    }
+
+    public void adicionarHorario(String horario) {
+        if (horario != null && !horario.isEmpty()) {
+            horarios.add(horario);
+        }
+    }
+
+    public List<String> getTiposExercicio() {
+        return tiposExercicio;
+    }
+
+    public List<String> getHorarios() {
+        return horarios;
+    }
 }
