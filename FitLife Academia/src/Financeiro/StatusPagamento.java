9bcaba13140159261adb08b06ab65d900924c09a
ksouvu <canutoisael@gmail.com>
2025-11-22 22:52:48 -0300

    Update StatusPagamento.java

--- FitLife Academia/src/Financeiro/StatusPagamento.java
+++ FitLife Academia/src/Financeiro/StatusPagamento.java
@@ -1,7 +1,7 @@
 package Financeiro;
 
 public enum StatusPagamento {
-    PENDENTE, // Fatura gerada, mas ainda não paga.
-    PAGO,     // Fatura liquidada.
-    ATRASADO; // Fatura vencida.
+    PENDENTE, 
+    PAGO,     
+    ATRASADO; 
 }
